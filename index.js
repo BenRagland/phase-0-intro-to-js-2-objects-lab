535f0cd0f6510c8e816fe7aed451cec7b65b3bf0
Benjamin <Officialbenragland@gmail.com>
2024-02-10 11:52:19 -0500

    Adds test solutions to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee ={
+    name:"",
+    streetAddress:""
+
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {
+        ...employee,
+        [key]:value,
+
+    }
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value
+    return employee
+        
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    
+    const {[key]:value,...restOfObj} = employee
+    return restOfObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    
+     delete employee[key]
+     return employee
+}
+
